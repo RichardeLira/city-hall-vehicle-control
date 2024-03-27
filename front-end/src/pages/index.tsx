759bc6b694640e9a509792e9c405050d44584f55
Pedro Grigorio <pedrooh.hxh@gmail.com>
2024-03-27 14:19:40 -0300

    first navigation

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div className="text-black">Hello World</div>
+  return (
+    <div className="flex flex-col text-black">
+      <span>Hello World</span>
+      <button>
+        <Link href="/municipal-vehicles">Municipal Vehicles</Link>
+      </button>
+    </div>
+  )
 }
