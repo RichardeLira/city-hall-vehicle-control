9a04206f696f4fb5534181824f94fc10dec6f740
Pedro Grigorio <pedrooh.hxh@gmail.com>
2024-03-31 23:16:11 -0300

    finish filters implementation

--- front-end/src/components/ui/SortFilter/index.tsx
+++ front-end/src/components/ui/SortFilter/index.tsx
@@ -1,11 +1,8 @@
-import * as React from 'react'
-
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from '@/lib/shadcnui/select'
@@ -13,27 +10,30 @@ import {
 interface FilterProps {
   placeholder: string
   options: string[]
-  selectedOption: string
-  setSelectedOption: (option: string) => void
+  selectedOption: string | null
+  setSelectedOption: (option: string | null) => void
 }
 
-export default function Filter({
+export default function SortFilter({
   placeholder,
   options,
   selectedOption,
   setSelectedOption,
 }: FilterProps) {
   return (
-    <Select
-      value={selectedOption}
-      onValueChange={(option) => setSelectedOption(option)}
-    >
-      <SelectTrigger className="w-[180px] focus:ring-offset-0 focus:ring-0">
-        <SelectValue placeholder={placeholder} />
+    <Select onValueChange={(option) => setSelectedOption(option)}>
+      <SelectTrigger className="w-[240px] focus:ring-offset-0 focus:ring-0">
+        {selectedOption ? (
+          <SelectValue>
+            <span className="text-gray-500">Ordenar por: {''}</span>
+            <span>{selectedOption}</span>
+          </SelectValue>
+        ) : (
+          <span className="text-gray-500">Ordenar por: {placeholder}</span>
+        )}
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
-          <SelectLabel>{placeholder}</SelectLabel>
           {options.map((option) => (
             <SelectItem key={option} value={option}>
               {option}
--- front-end/src/pages/vehicle-control/leased-vehicles/index.tsx
+++ front-end/src/pages/vehicle-control/leased-vehicles/index.tsx
@@ -1,8 +1,20 @@
+import CustomButton from '@/components/ui/Button'
 import Filter from '@/components/ui/Filter'
 import Searchbox from '@/components/ui/Searchbox'
-import { Teste } from '@/components/ui/Teste'
+import SortFilter from '@/components/ui/SortFilter'
+import { useState } from 'react'
 
 export default function LeasedVehicles() {
+  const [sortFilter, setSortFilter] = useState<string | null>(null)
+  const [ownerFilter, setOwnerFilter] = useState<string>('')
+  const [yearFilter, setYearFilter] = useState<string>('')
+
+  const resetFilters = () => {
+    setSortFilter(null)
+    setOwnerFilter('')
+    setYearFilter('')
+  }
+
   const handleSearch = (query: string) => {
     console.log(query)
   }
@@ -14,11 +26,34 @@ export default function LeasedVehicles() {
           placeholder="Procurar por descrição do veículo"
           onSearch={handleSearch}
         />
-        <div className="flex gap-2 mt-4 items-center">
-          <h1 className="text-2xl">Veículos Locados</h1>
-          <Teste />
-          {/* <Filter />
-          <Filter /> */}
+        <div className="flex mt-4 justify-between items-center">
+          <div className="flex gap-2 items-center">
+            <h1 className="text-2xl">Veículos Locados</h1>
+            <Filter
+              placeholder="Ano"
+              options={['2010', '2012', '2013', '2014', '2015']}
+              selectedOption={yearFilter}
+              setSelectedOption={setYearFilter}
+            />
+            <Filter
+              placeholder="Proprietário"
+              options={['Maria', 'João', 'Fernando', 'José', 'Enaldo']}
+              selectedOption={ownerFilter}
+              setSelectedOption={setOwnerFilter}
+            />
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <CustomButton onClick={resetFilters}>
+              Redefinir Filtros
+            </CustomButton>
+            <SortFilter
+              placeholder="-"
+              options={['Ano', 'Marca', 'Proprietário']}
+              selectedOption={sortFilter}
+              setSelectedOption={setSortFilter}
+            />
+          </div>
         </div>
       </div>
       {/* Table */}
